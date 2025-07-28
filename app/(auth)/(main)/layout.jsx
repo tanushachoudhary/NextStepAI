b1467a1e42a501fa39718e4b9c17f8437f5247cb
Tanusha Choudhary <144540796+tanushachoudhary@users.noreply.github.com>
2025-07-28 17:48:02 +0530

    Delete app/(auth) directory

--- app/(auth)/(main)/layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainLayout = async ({ children }) => {
-  return <div className="container mx-auto mt-24 mb-20">{children}</div>;
-};
-
-export default MainLayout;
